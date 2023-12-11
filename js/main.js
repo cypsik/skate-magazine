62a5218b430a5b85459aebdadd85a567f01bface
Cypsik <137959960+cypsik@users.noreply.github.com>
2023-12-11 19:40:11 +0100

    finishing the navigation

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const navItems = document.querySelector('.nav-items');
 const navItemsBtn = document.querySelector('.nav-btn');
 const allNavItems = document.querySelectorAll('.nav-items__item');
 
+
 const handleNav = () => {
 	navItems.classList.toggle('nav-active');
 
@@ -10,6 +11,26 @@ const handleNav = () => {
 			navItems.classList.remove('nav-active');
 		});
 	});
+
+	handleNavItemsAnimation()
+	deleteAnimation()
+};
+
+const handleNavItemsAnimation = () => {
+	allNavItems.forEach((item, index) => {
+		item.classList.toggle('nav-items-animation');
+		item.style.animationDelay = (index-1)*0.1 + 's';
+	});
 };
 
 navItemsBtn.addEventListener('click', handleNav);
+
+const deleteAnimation = () => {
+    allNavItems.forEach(item => {
+		item.addEventListener('click', () => {
+			allNavItems.forEach(el => {
+				el.classList.remove('nav-items-animation')
+			})	
+		})
+    })
+}
