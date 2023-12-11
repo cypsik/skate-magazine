75fd7b8fbecaf33cfcb279db3fdba1d634242aa3
Cypsik <137959960+cypsik@users.noreply.github.com>
2023-12-11 20:53:38 +0100

    making cleaner code for the nab-btn bars, starting adding js for changing colors at features section

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 const navItems = document.querySelector('.nav-items');
 const navItemsBtn = document.querySelector('.nav-btn');
+const navBars = document.querySelector('.nav-btn__bars');
 const allNavItems = document.querySelectorAll('.nav-items__item');
-
+const navLinks = document.querySelectorAll('.nav-links__link');
+const navLogo = document.querySelector('.nav__home');
+const whiteSection = document.querySelector('.white-section')
+// const footerYear = document.querySelector('.footeryear')
 
 const handleNav = () => {
 	navItems.classList.toggle('nav-active');
@@ -12,25 +16,33 @@ const handleNav = () => {
 		});
 	});
 
-	handleNavItemsAnimation()
-	deleteAnimation()
+	handleNavItemsAnimation();
+	deleteAnimation();
 };
 
 const handleNavItemsAnimation = () => {
 	allNavItems.forEach((item, index) => {
 		item.classList.toggle('nav-items-animation');
-		item.style.animationDelay = (index-1)*0.1 + 's';
+		item.style.animationDelay = (index - 1) * 0.1 + 's';
 	});
 };
 
-navItemsBtn.addEventListener('click', handleNav);
-
 const deleteAnimation = () => {
-    allNavItems.forEach(item => {
+	allNavItems.forEach((item) => {
 		item.addEventListener('click', () => {
-			allNavItems.forEach(el => {
-				el.classList.remove('nav-items-animation')
-			})	
-		})
-    })
+			allNavItems.forEach((el) => {
+				el.classList.remove('nav-items-animation');
+			});
+		});
+	});
+};
+
+const handleObserver = () => {
+	const currentSection = window.scrollY;
+
+	if (section.classList.contains('white-section') && section.offsetTop <= currentSection) {
+		
+	}
 }
+
+navItemsBtn.addEventListener('click', handleNav);
